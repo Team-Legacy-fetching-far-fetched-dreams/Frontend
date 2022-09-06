8086f31be6843a2729bbdd3548ff4bf160b75773
unknown <larbisammy18@gmail.com>
2022-09-06 13:07:30 +0000

    created login page

--- legacy/src/components/pages/Admin/AdminLogin.js
+++ legacy/src/components/pages/Admin/AdminLogin.js
@@ -1,12 +1,36 @@
-import React from 'react'
+import React,{useState} from 'react'
 import './AdminLogin.css'
 import Logo from '../../../imgs/logo2.png'
-import Button from 'react-bootstrap/Button';
 import {Link} from 'react-router-dom'
 import {motion} from 'framer-motion/dist/framer-motion'
 
 
+
 const AdminLogin = () => {
+  const [values,setValues] = useState({
+    userName:"",
+    password:"",
+
+  });
+
+  const [submit,setSubmit]=useState(false);
+  const [valid,setValid]= useState(false);
+
+  const handleuserName = (event) =>{
+    setValues({...values,userName: event.target.value})
+  }
+
+  const handlepassword = (event) =>{
+    setValues({...values,password: event.target.value})
+  }
+
+  const handleSubmit = (event) =>{
+    event.preventDefault();
+    if(values.userName&&values.password){
+      setValid(true);
+    }
+    setSubmit(true);
+  }
 
   return (
     <motion.div className='A-m'
@@ -19,15 +43,45 @@ const AdminLogin = () => {
        <div  className = "A-h">
            <img src={Logo} alt="" className = "A-logo"></img>  
        </div>
+
        <div className='Al-content'>
-            <h1>Welcome !</h1>
+            <h1>Welcome </h1>
+            <h2>Please Enter  Username And Password To Login</h2>
+           
+            <form className='form-container' onSubmit={handleSubmit}>
+              {submit && valid ? <div className='success-message'>You're Logged In</div> : null}
+              <input 
+              onChange={handleuserName}
+              value={values.userName}
+              className='form-field'
+              type="text"
+              name="username"
+              placeholder='username'
+              required
+              />
+              <br></br>
+             {submit && !values.userName?<span>Please enter your username</span>:null}
+             <br></br>
+              <input 
+              onChange={handlepassword}
+              value={values.password}
+              className='form-field'
+              type="password"
+              placeholder='password'
+              required
+              />
+              <br></br>
+              {submit && !values.password ? <span>Please enter your password</span> : null}
+              
+              <br></br>
+                <button className='login-btn' >Login</button>
+            </form>
 
 
-          <Link to ="/AdminDashboard">
-             <Button className='a-login' active>Login</Button>
-          </Link>
+         {/* <div className='text'>forgot password?</div> */}
       </div>
        </div>
+       
     </motion.div>
 
   )
