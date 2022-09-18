c7e141df625cabe321e8783511d8ad68fc215a69
Osei Kofi Frimpong <55978137+oseikf822@users.noreply.github.com>
2022-09-19 04:01:04 +0800

    updated aboutus

--- legacy/src/components/pages/LandingPage/AboutUs.js
+++ legacy/src/components/pages/LandingPage/AboutUs.js
@@ -1,20 +1,60 @@
 import React from 'react'
 import './AboutUs.css'
 import {motion} from 'framer-motion/dist/framer-motion'
+import Logo from '../../../imgs/logo2.png'
+import Back from '../../../imgs/Doc.png'
 
-const AboutUs = () => {
-  return (
-    <motion.div className='Ab-m'
-    initial={{opacity: 0}}
-    animate={{opacity: 1}}
-    exit={{opacity: 0}}
+const AboutUs = () => 
+<div className='Ab-m'>
+<div className='Ab-g'>
+<div className = "d-h">
+     <img src={Logo} alt="" className = "D-logo"></img>  
+  </div>
+  
+  <h2 className='abtus'> About Us</h2>
+  <div className='header'>
+
+    <div class="cnt" >
+    <img src={Back} alt="" className='back-image'></img>
+    <h1 className='leg' >Legacy HealthCare</h1>
+    <h2 className='farf'>Fetching Far Fetched Dreams</h2>
     
-    >
-    <div className='Ab-g'>
+      <h2 className='farfy'>
+        Introduction
+      </h2>
+        
+      <p className='p11'>
+      The Legacy HealthCare Centre(LHC) is located in Kumasi, the capital of the Ashanti region.
+
+The geographical location of the 300-bed hospital, the road network of the country and the unique commercial setting of Kumasi makes the hospital accessible to all remaining fifteen (15) regions of the country, especially those that shares boundaries with the Ashanti region.
+
+The hospital currently has twelve (12) clinical directorates and two (2) non-clinical support services directorates.
+      </p>
+      <h2 className='farf'>
+        Our History
+      </h2>
+      <p className='p11'>
       
-      </div>
-    </motion.div>
-  )
-}
+
+
+
+The new hospital complex was completed by the contractors, Messrs. GEE Walter and named Legacy HealthCare Centre(LHC) 
+
+The hospital attained a teaching hospital status for the clinical training of medical students following the establishment of the School of Medical Sciences (now School of Medicine and Dentistry, SMD) of the Kwame Nkrumah University of Science and Technology.
+
+Legacy HealthCare Centre is currently a major centre for the training of postgraduate medical and dental practitioners in various specialties and the training of undergraduate personnel in Pharmacy, Nursing and many Allied Health Professions.
+
+Through its Directorates and Units, many research works on health and health-related areas have been undertaken, many of which have been published in renowned and peer-reviewed national and international journals.
+      </p>
+      
+      {/* <a href="#" class="btn">Get In Touch </a> */}
+      
+      
+          
+</div>
+  </div>
+</div>
+</div>
+
 
 export default AboutUs
--- legacy/src/components/pages/LandingPage/ContactUs.js
+++ legacy/src/components/pages/LandingPage/ContactUs.js
@@ -1,20 +1,47 @@
 import React from 'react'
 import './ContactUs.css'
 import {motion} from 'framer-motion/dist/framer-motion'
+import Logo from '../../../imgs/logo2.png'
+
 
 const ContactUs = () => {
   return (
-    <motion.div className='C-m'
-    initial={{opacity: 0}}
-    animate={{opacity: 1}}
-    exit={{opacity: 0}}
-    
-    >
+    <div className='C-m'>
     <div className='C-g'>
-      
-      </div>
-    </motion.div>
-  )
-}
+    <div className = "d-h">
+        <img src={Logo} alt="" className = "D-logo"></img>  
+     </div>
+    
+       <p></p>
+           <div className='chester'>
+           <p  className='pte'>
+           Welcome to our Website. We are glad to have you around.
+           </p>
+                  <div className='flexy'>
+                   <div>
+                   <h2 className='boxy'> Contact Us</h2>
+                     <p className='p11'>Address: P.O.Box 1934 Adum-Kumasi</p>
+                     <p className='p11'>Hotlines: +233 555 874 197 /
+                       +233 598 502 203</p>
+                     <p className='p11'>Email: ksosei555@gmail.com</p>
+                   </div>
+                   <div className='wemove'>
+                     <h2 className='boxy'>Connect With Us</h2>
+                     <p className='p11'>Digital Address: AK-082-626-0</p>
+                   </div>
+                  </div>
+
+
+
+
+
+           {/* <a href="#" class="btn">Get In Touch </a> */}
+           </div>
+    </div>
+   </div>
+ )
+} 
+
+
 
 export default ContactUs
