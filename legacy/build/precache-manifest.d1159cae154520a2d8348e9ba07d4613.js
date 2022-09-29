42a5c6699070863d77a70c36ff61df2d11f3595b
Essuman Godsaves <kojosayves7@gmail.com>
2022-09-29 09:19:49 +0000

    included build folder

---
+++ legacy/build/precache-manifest.d1159cae154520a2d8348e9ba07d4613.js
@@ -0,0 +1,158 @@
+self.__precacheManifest = [
+  {
+    "revision": "f272e24878a65b789256",
+    "url": "/static/css/main.d56b35f0.chunk.css"
+  },
+  {
+    "revision": "f272e24878a65b789256",
+    "url": "/static/js/main.3415479a.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "8fb14d3e7174b44d650d",
+    "url": "/static/css/2.31156a4d.chunk.css"
+  },
+  {
+    "revision": "8fb14d3e7174b44d650d",
+    "url": "/static/js/2.11e3e931.chunk.js"
+  },
+  {
+    "revision": "2b82802f37903c9b4fbdb990a5f5c732",
+    "url": "/static/media/logo2.2b82802f.png"
+  },
+  {
+    "revision": "945ae3a8be44aeece2f95fe01e5f9612",
+    "url": "/static/media/nipp.945ae3a8.png"
+  },
+  {
+    "revision": "eb12d692e0817585b25ae9ba4589d0d4",
+    "url": "/static/media/Doc.eb12d692.png"
+  },
+  {
+    "revision": "c8f6bd858cde9edfe83cb861b129273c",
+    "url": "/static/media/415.c8f6bd85.jpg"
+  },
+  {
+    "revision": "d0aa71efb2e063e9d1faa0572d64feab",
+    "url": "/static/media/NewProfile.d0aa71ef.png"
+  },
+  {
+    "revision": "edf41d4d3bcf0b15b112a516c6a83558",
+    "url": "/static/media/Log.edf41d4d.png"
+  },
+  {
+    "revision": "765943122e3cb5ffa4d947746fadc367",
+    "url": "/static/media/security.76594312.png"
+  },
+  {
+    "revision": "72f37dd5fc027798f89d243b979869d8",
+    "url": "/static/media/comp.72f37dd5.png"
+  },
+  {
+    "revision": "54781e40cd51da2306bfe5879b1297d4",
+    "url": "/static/media/Admin1.54781e40.png"
+  },
+  {
+    "revision": "a1220f8ac99a2f461aeeab811b405852",
+    "url": "/static/media/doc5.a1220f8a.png"
+  },
+  {
+    "revision": "f975edaf9590531739c0369c5263fcf3",
+    "url": "/static/media/Nurse1.f975edaf.png"
+  },
+  {
+    "revision": "85ab35497e44e0d8ab91f8bd16549a98",
+    "url": "/static/media/object1.85ab3549.png"
+  },
+  {
+    "revision": "89841bb9585d8f89d98c520d336124b4",
+    "url": "/static/media/object2.89841bb9.png"
+  },
+  {
+    "revision": "4f88c8b8b8fbb21bc25ce8f90277c6cc",
+    "url": "/static/media/object3.4f88c8b8.png"
+  },
+  {
+    "revision": "4908fc2251507956d68097101ad09015",
+    "url": "/static/media/object4.4908fc22.png"
+  },
+  {
+    "revision": "a9e4af70be94c4d05489b9e0505c003f",
+    "url": "/static/media/object5.a9e4af70.png"
+  },
+  {
+    "revision": "14f115f0914ebb3ef7220c5706c3be4d",
+    "url": "/static/media/Artboard 1.14f115f0.png"
+  },
+  {
+    "revision": "c0cf517842480854f257efc5487bbeeb",
+    "url": "/static/media/patient image.c0cf5178.png"
+  },
+  {
+    "revision": "01d6c66d97a36c7c115542a28dea492a",
+    "url": "/static/media/doc1.01d6c66d.png"
+  },
+  {
+    "revision": "342d44ebf5b840e1aa02639a65112d58",
+    "url": "/static/media/doc2.342d44eb.png"
+  },
+  {
+    "revision": "d941ba1ae9f5d919ed1477d1266eaa58",
+    "url": "/static/media/doc3.d941ba1a.png"
+  },
+  {
+    "revision": "614c926c747abeb9c7b1196e191cde0f",
+    "url": "/static/media/doc4.614c926c.png"
+  },
+  {
+    "revision": "6a274b27a78f65dd72bf9f3def3e315d",
+    "url": "/static/media/Nurse2.6a274b27.png"
+  },
+  {
+    "revision": "45e57334d2f81b0d554cbfe81b6dbc99",
+    "url": "/static/media/Nurse3.45e57334.png"
+  },
+  {
+    "revision": "6303b7abbd0861320df4b532d3957339",
+    "url": "/static/media/Nurse4.6303b7ab.png"
+  },
+  {
+    "revision": "68d4cd56552b715b6c2e972926a62ea5",
+    "url": "/static/media/Admin2.68d4cd56.png"
+  },
+  {
+    "revision": "e831d0b6105b1966b3c0a96b6ae31253",
+    "url": "/static/media/Admin3.e831d0b6.png"
+  },
+  {
+    "revision": "e91dc2b5044bcfb42326390d34ef25bd",
+    "url": "/static/media/Admin4.e91dc2b5.png"
+  },
+  {
+    "revision": "27819a663cc45d525c675af5e1f6f943",
+    "url": "/static/media/wel2.27819a66.jpg"
+  },
+  {
+    "revision": "0f6cc951a24e4803c25a1f55cc21d190",
+    "url": "/static/media/login1.0f6cc951.png"
+  },
+  {
+    "revision": "6adcdd782905f5b07ca63e9ce0bb719f",
+    "url": "/static/media/hellonurse.6adcdd78.jpg"
+  },
+  {
+    "revision": "0102ced612752a7b10706d50c1c66586",
+    "url": "/static/media/loginpin.0102ced6.png"
+  },
+  {
+    "revision": "582ade8a689ef16267c1ce2c28dafd08",
+    "url": "/static/media/nursesignup.582ade8a.jpg"
+  },
+  {
+    "revision": "75a10090bddfe7c6dffbb499c0593516",
+    "url": "/index.html"
+  }
+];
